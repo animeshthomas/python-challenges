fe46fde0e7759db738381896cf7c60e1831f8eaf
Animesh Thomas <animeshthomas262@gmail.com>
2024-02-21 23:54:12 +0530

    challage 6 done

---
+++ string challanges/max_occurence_of_a_substring.py
@@ -0,0 +1,14 @@
+def maxOccurenceofASubstring(s, sub_list):
+    max_count = 0
+    max_sub = ""
+    for letters in sub_list:
+        count=s.count(letters)
+        if(count>=max_count):
+            max_count=count
+            max_sub=letters
+    return max_sub
+
+test_str = "gfghsisbjknlmkesbestgfgsdcngfgcsdjnisdjnlbestdjsklgfgcdsbestbnjdsgfgdbhisbhsbestdkgfgb"
+test_list = ['gfg', 'is', 'best']
+
+print("The maximum run of Substring:", maxOccurenceofASubstring(test_str, test_list))
