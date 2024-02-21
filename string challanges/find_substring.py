80ad43639194f2a7e664eb6f48f158c2696dcfd2
Animesh Thomas <animeshthomas262@gmail.com>
2024-02-22 00:10:04 +0530

    challage 7 done

--- string challanges/find_substring.py
+++
@@ -1,8 +0,0 @@
-def findSubString(string,substring):
-    s=string.split()
-    print(s)
-    if substring in string:
-        return True
-    else:
-        return False
-print(findSubString("Hello World","Hello"))
